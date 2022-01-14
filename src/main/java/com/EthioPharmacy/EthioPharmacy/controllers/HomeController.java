97f95b96597fbb8d580961459f12740abbc14ecd
bamlakhiruya@gmail.com <bamlakuhiruya@gmail.com>
2022-01-14 15:13:26 -0800

    allowing a user to add a category

--- src/main/java/com/EthioPharmacy/EthioPharmacy/controllers/HomeController.java
+++ src/main/java/com/EthioPharmacy/EthioPharmacy/controllers/HomeController.java
@@ -1,27 +1,40 @@
 package com.EthioPharmacy.EthioPharmacy.controllers;
 
 
+import com.EthioPharmacy.EthioPharmacy.models.Medicine;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
 @Controller
-@RequestMapping(path = "/home")
+@RequestMapping(path = "home")
 public class HomeController {
-
+    static ArrayList<Medicine> medicines = new ArrayList<>();
     @GetMapping
     public String showHome(Model model)
     {
-        ArrayList<String> medicines = new ArrayList<>();
-        medicines.add("Aspirin");
-        medicines.add("methamin");
-        medicines.add("thermophin");
-        medicines.add("therm");
+
+
         model.addAttribute("title", "OurProducts");
         model.addAttribute("medicines", medicines);
-        return "/home";
+        return "meds/home";
+    }
+
+    @GetMapping(path = "/add")
+    public String displayAddForm(Model model) {
+        model.addAttribute("title", "Add Medicine");
+        return "meds/addMedForm";
+    }
+
+    @PostMapping("/add")
+    public String processAddForm(@RequestParam String medName, @RequestParam String medPrice, @RequestParam String medDescription) {
+        Medicine newMed = new Medicine( medName, medPrice, medDescription);
+        medicines.add(newMed);
+        return "redirect:/home";
     }
 }
---
+++ src/main/java/com/EthioPharmacy/EthioPharmacy/models/Medicine.java
@@ -0,0 +1,10 @@
+package com.EthioPharmacy.EthioPharmacy.models;
+
+import lombok.Data;
+
+@Data
+public class Medicine {
+    private final String medName;
+    private final String medPrice;
+    private final String medDescription;
+}
