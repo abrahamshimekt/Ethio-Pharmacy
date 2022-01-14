7ece2867f386c04e442be74ddf776d2ab528c9cd
bamlakhiruya@gmail.com <bamlakuhiruya@gmail.com>
2022-01-14 04:22:15 -0800

    editing the homepage

---
+++ src/main/java/com/EthioPharmacy/EthioPharmacy/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.EthioPharmacy.EthioPharmacy.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/home")
+public class HomeController {
+
+    @GetMapping
+    public String showHome() {
+        return "med/home";
+    }
+}
