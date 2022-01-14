7b62a49aab9b7f7318e159792f15124c77c5f03e
bamlakhiruya@gmail.com <bamlakuhiruya@gmail.com>
2022-01-14 09:00:42 -0800

    adding categories

--- src/main/java/com/EthioPharmacy/EthioPharmacy/controllers/HomeController.java
+++ src/main/java/com/EthioPharmacy/EthioPharmacy/controllers/HomeController.java
@@ -2,15 +2,26 @@ package com.EthioPharmacy.EthioPharmacy.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping(path = "/home")
 public class HomeController {
 
     @GetMapping
-    public String showHome() {
-        return "med/home";
+    public String showHome(Model model)
+    {
+        ArrayList<String> medicines = new ArrayList<>();
+        medicines.add("Aspirin");
+        medicines.add("methamin");
+        medicines.add("thermophin");
+        medicines.add("therm");
+        model.addAttribute("title", "OurProducts");
+        model.addAttribute("medicines", medicines);
+        return "/home";
     }
 }
