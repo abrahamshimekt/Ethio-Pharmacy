e3371851ae4b4fd3d3eac3b2693921c996ce27a8
Hiwot Derese <hiwotderese16@gmail.com>
2022-01-17 13:09:13 +0300

    edit the bootstap class

--- src/main/java/com/EthioPharmacy/EthioPharmacy/EthioPharmacyApplication.java
+++ src/main/java/com/EthioPharmacy/EthioPharmacy/EthioPharmacyApplication.java
@@ -2,10 +2,6 @@ package com.EthioPharmacy.EthioPharmacy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class EthioPharmacyApplication {
@@ -14,14 +10,4 @@ public class EthioPharmacyApplication {
 		SpringApplication.run(EthioPharmacyApplication.class, args);
 	}
 
-	@Controller
-	@RequestMapping( path = "/home")
-	public static class HomeController {
-
-		@GetMapping("/")
-		public String showHome(Model model) {
-
-			return "index";
-		}
-	}
 }
