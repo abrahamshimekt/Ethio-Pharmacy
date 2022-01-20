72d1a870139491dc04d2e84887160d646b40da38
Rut Birhanu <birhanurut@gmail.com>
2022-01-20 12:55:37 +0300

    adding required fields

---
+++ src/main/java/com/EthioPharmacy/EthioPharmacy/models/Role.java
@@ -0,0 +1,18 @@
+package com.EthioPharmacy.EthioPharmacy.models;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
--- src/main/java/com/EthioPharmacy/EthioPharmacy/models/User.java
+++ src/main/java/com/EthioPharmacy/EthioPharmacy/models/User.java
@@ -1,5 +1,33 @@
 package com.EthioPharmacy.EthioPharmacy.models;
 
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.Collection;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String company;
+    private String address;
+    private  String email;
+    private String phone;
+    private String password;
+
+    @ManyToMany(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
+   
+    private Collection<Role> roles;
+
 }
